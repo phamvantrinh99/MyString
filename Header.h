f1c0112117281cb735576e1d95431f9db0621624
phamvantrinh99 <37787375+phamvantrinh99@users.noreply.github.com>
2018-12-04 17:03:49 +0700

    Update Header.h

--- Header.h
+++ Header.h
@@ -10,12 +10,10 @@ public:
 public:
 	MYSTRING();
 	MYSTRING(const MYSTRING &s);
-	MYSTRING(const string s);
 	MYSTRING(const char *s);
 	MYSTRING(int len);
 	~MYSTRING();
 	MYSTRING& operator=(const MYSTRING&s);
-	MYSTRING& operator=(const string&str);
 	MYSTRING& operator=(const char* c);
 	char* begin();
 	char *end();
@@ -33,7 +31,23 @@ public:
 	char back();
 	char front();
 	MYSTRING& operator+=(const MYSTRING&s);
-	MYSTRING& operator+(const string&s);
 	MYSTRING& operator+=(const char*s);
+	MYSTRING& append(const MYSTRING&s);
+	MYSTRING& append(const MYSTRING &s, int pos, int n);
+	MYSTRING& append(const char *s);
+	MYSTRING& append(const char*s, int n);
+	MYSTRING& append(int n, char c);
+	void push_back(char c);
+	MYSTRING& assign(const MYSTRING&s);
+	MYSTRING& assign(const MYSTRING&s, int pos, int n);
+	MYSTRING& assign(const char*s);
+	MYSTRING& assign(const char*s, int n);
+	MYSTRING& assign(int n, char c);
+	MYSTRING& insert(int pos, const MYSTRING&s);
+	MYSTRING& insert(int pos, const MYSTRING&s, int subpos, int n);
+	MYSTRING& insert(int pos, const char*s);
+	MYSTRING& insert(int pos, const char*s, int n);
+	MYSTRING& insert(int pos, int n, char c);
+	MYSTRING& erase(int pos, int n);
 };
 #endif
